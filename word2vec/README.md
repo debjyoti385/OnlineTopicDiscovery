954b6a8a0f25ec8ee4d7bc5a4bf53e70e900845b
Debjyoti Paul <debjyotipaul385@gmail.com>
2016-12-08 00:19:47 -0700

    code refactor

---
+++ word2vec/README.md
@@ -0,0 +1,2 @@
+# Model 
+Here we store the model. Make sure this directory available to vectorizer code.
--- word2vec/vectorize_tweets.py
+++ word2vec/vectorize_tweets.py
@@ -2,39 +2,57 @@ from gensim.models.doc2vec import LabeledSentence
 import os
 import json
 import gensim.models
+import re
+import argparse
 #rootdir = '../../../deb/tweets_with_county'
 rootdir = "../data/"
+MODEL_LOCATION ='model/tweet_model.doc2vec'
 
+text_regex =re.compile('\s*\"text\":\s*\"([^"]+)\"')
 
 class MySentences(object):
-	def __init__(self, dirname):
-        	self.dirname = dirname
-	
-	def __iter__(self):
-		print 'Reading files'
-		for subdir, dirs, files in os.walk(self.dirname):
-			for fn in files:
-				print os.path.join(subdir,fn)
-				for line_ind,line in enumerate(open(os.path.join(subdir,fn))):
-					line_data = json.loads(line)
-					print 'The tag is - SENT_%s' % (fn+'-'+str(line_ind))
-					yield LabeledSentence(words=line_data["text"].split(),tags=['SENT_%s' % (fn+'-'+str(line_ind))])
+    def __init__(self, dirname):
+        self.dirname = dirname
+    
+    def __iter__(self):
+        print 'Reading files'
+        for subdir, dirs, files in os.walk(self.dirname):
+            for fn in files:
+                print os.path.join(subdir,fn)
+                for line_ind,line in enumerate(open(os.path.join(subdir,fn))):
+                    #line_data = json.loads(line)
+                    text = re.findall(text_regex,line)
+                    #print text[0]
+                    #print 'The tag is - SENT_%s' % (fn+'-'+str(line_ind))
+                    yield LabeledSentence(words=text[0].split(),tags=['SENT_%s' % (fn+'-'+str(line_ind))])
 
-print 'Program started'
-'''
-sentences = MySentences(rootdir)
-model = gensim.models.Doc2Vec(alpha=0.025, min_alpha=0.025, min_count=1)
-model.build_vocab(sentences)
-print 'Completed building vocab'
-for epoch in range(1):
-	model.train(sentences)
-	model.alpha -= 0.002
-	model.min_alpha = model.alpha
 
-print 'Training done'
-model.save('/tmp/my_model.doc2vec')
-'''
-model_loaded = gensim.models.Doc2Vec.load('/tmp/my_model.doc2vec')
-if model_loaded:
-	print 'Loaded'
-print 'Vector is - ',model_loaded.docvecs["SENT_part-02753-372"],' || Length of vector is - ',len(model_loaded.docvecs)
+
+def main(model=False):
+    print 'Program started'
+    if model:
+        sentences = MySentences(rootdir)
+        model = gensim.models.Doc2Vec(alpha=0.025, min_alpha=0.025, min_count=1)
+        model.build_vocab(sentences)
+        print 'Completed building vocab'
+        for epoch in range(1):
+            model.train(sentences)
+            model.alpha -= 0.002
+            model.min_alpha = model.alpha
+        
+        print 'Training done \nStoring model at location ', MODEL_LOCATION
+        model.save(MODEL_LOCATION)
+
+    model_loaded = gensim.models.Doc2Vec.load(MODEL_LOCATION)
+    if model_loaded:
+        print 'Loaded'
+    print 'Vector is - ',model_loaded.docvecs["SENT_part-02753-372"],' || Length of vector is - ',len(model_loaded.docvecs)
+
+
+if __name__=="__main__":
+    parser = argparse.ArgumentParser(
+                            description='Vectorizer for Online Topic Detection')
+    parser.add_argument(
+                    '-c','--create', help='Train and create model ', default= False, required=False, action='store_true')
+    args = parser.parse_args()
+    main(model=args.create)
