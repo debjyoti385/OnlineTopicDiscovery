7c34bcb95edf9bff70001554e7f4c53d98df8688
Debjyoti Paul <debjyotipaul385@gmail.com>
2016-12-13 07:10:20 -0700

    modified Kmeans arguments

--- src/onlineKMeans.py
+++ src/onlineKMeans.py
@@ -28,7 +28,7 @@ class TweetStream(object):
         self.dirname = dirname
     
     def __iter__(self):
-        print 'Reading files'
+        #print 'Reading files'
         for subdir, dirs, files in os.walk(self.dirname):
             for fn in files:
                 print os.path.join(subdir,fn)
@@ -63,16 +63,20 @@ class OnlineKMeans(object):
     cluster_means = {}
     points =[]   
     size = 0
+    norm=2
     
-    def __init__(self,k_target,vector_length, init_delta=15):
-     	print 'In init method'
+    def __init__(self,k_target,vector_length, norm=2, init_delta=15):
+     	print 'Initialize Online K Means with k ', str(k_target) , " and distance norm ", str(norm)
         self.k_target = k_target
+        self.norm = norm
         if k_target < 15 and k_target > 3:
             init_delta = 3
         self.k = ceil( (k_target - init_delta)/5 ) 
         self.vector_length = vector_length
     
     def point_distance(self,vector, point):
+        if self.norm != 2:
+            return np.linalg.norm(vector-point,ord=self.norm)
         return np.linalg.norm(vector - point)
 
     def cluster_distance(self,vector, cluster_num):
@@ -92,13 +96,13 @@ class OnlineKMeans(object):
         min_distances.sort(key=lambda d:d[1])
         min_value = min_distances[0][1]
         cluster_num = min_distances[0][0]
-        print min_distances 
+        #print min_distances 
         for i in range(min(len(min_distances),3)):
             temp = self.cluster_distance(vector, min_distances[i][0])
             if temp < min_value:
                 cluster_num = min_distances[i][0]
                 min_value= temp
-        print min_value
+        #print min_value
         return min_value, cluster_num
             
     def get_nearest_cluster(self,vector):
@@ -154,7 +158,7 @@ class OnlineKMeans(object):
         else:
             distance, cluster_num =  self.D(vector)
             print distance
-            print self.f[self.r]
+            #print self.f[self.r]
             probability = min(pow(distance,2)/self.f[self.r],1.0)
             print probability
             if self.isTrue(probability):
@@ -186,12 +190,12 @@ def extract_tweet(line):
     return text[0]
 
 
-def main(k_target,datadir, model_path):
+def main(k_target,datadir, model_path, norm=2, outputfile="output.json"):
     tweets = TweetStream(datadir)
     vectorizer = gensim.models.Doc2Vec.load(model_path)
     vector_length = len(vectorizer.docvecs[0])
 
-    online_k_means = OnlineKMeans(k_target=20, vector_length=vector_length)
+    online_k_means = OnlineKMeans(k_target=20, vector_length=vector_length, norm=norm)
     count  = 0
     for tweet in tweets: 
         count +=1
@@ -205,17 +209,17 @@ def main(k_target,datadir, model_path):
         vector = vectorizer.infer_vector(words)
 
         online_k_means.add(20*vector,text=tweet)
+        print count
         if count % 500 ==0:
-            print tweet, vector
             print "PROCESSED "+ str(count) + " TWEETS"
-    with open("clusters.json",'w') as output:
+    with open(outputfile,'w') as output:
         result ={}
         result["name"] ="cluster"
         children =[]
         for key in online_k_means.clusters_text:
             temp={}
             temp["name"] = key
-            temp["children"]=[ {"name":point} for point in online_k_means.clusters_text[key]]
+            temp["children"]=[ {"name":point, "size":1} for point in online_k_means.clusters_text[key]]
             children.append(temp)
 
         result["children"]=children
@@ -229,7 +233,11 @@ if __name__ =="__main__":
     parser.add_argument(
                     '-i', '--input',help='Input data directory',required=True, type=str)
     parser.add_argument(
+                    '-o', '--output',help='output cluster json file ',default="clusters.json", required=False, type=str)
+    parser.add_argument(
                     '-m', '--model',help='Doc2Vec model location',required=False, default="../vectorizer/model/tweet_model.doc2vec", type=str)
+    parser.add_argument(
+                    '-l','--l', type=int, help='Distance Norm ', default=2, required=False)
    
     args = parser.parse_args()
-    main(args.k, args.input, args.model)
+    main(args.k, args.input, args.model, norm=args.l, outputfile=args.output)
